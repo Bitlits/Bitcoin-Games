e737a646ca9b505c14d2f020ed7839ac45fe9d31
Justin Faler <jfaler216@student.msjc.edu>
2019-02-23 15:58:23 -0800

    Update info.php

--- app/api/game/info.php
+++ app/api/game/info.php
@@ -1,4 +1,9 @@
 <?php
+/*
+🦉 Justin Faler
+🧪 https://github.com/Jfaler
+🧠 Base 64: UHJvcGVydHkgb2YgQml0bGl0cy5jb20gLyBQbGVhc2UgY29udGFjdCBpbW1lZGlhdGVseSBpZiB0aGlzIHByb2R1Y3QgaXMgdW5saWNlbnNlZC4=
+*/
 
 cors();
 
@@ -92,5 +97,5 @@ if(is_object($game)){
 }
 
 echo json_encode($output);
-
+// 🧬 Property of Bitlits.com ©2013-2019
 ?>
