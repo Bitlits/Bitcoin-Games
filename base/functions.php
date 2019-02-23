f37c6ae2b18e4933efdb9eeac4488751a6fc21f8
Justin Faler <jfaler216@student.msjc.edu>
2019-02-23 15:54:48 -0800

    Update functions.php

--- base/functions.php
+++ base/functions.php
@@ -1,4 +1,9 @@
 <?php
+/*
+🦉 Justin Faler
+🧪 https://github.com/Jfaler
+🧠 Base 64: UHJvcGVydHkgb2YgQml0bGl0cy5jb20gLyBQbGVhc2UgY29udGFjdCBpbW1lZGlhdGVseSBpZiB0aGlzIHByb2R1Y3QgaXMgdW5saWNlbnNlZC4=
+*/
 
 function getUniqueKey(){
 	return sha1(uniqid());
@@ -36,5 +41,5 @@ function cors() {
 	}
 	header('Content-type: application/json');
 }
-
+// 🧬 Property of Bitlits.com ©2013-2019
 ?>
