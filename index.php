a4abf32a124b8b8aa44eac664e47bc712571132b
Justin Faler <jfaler216@student.msjc.edu>
2019-02-23 15:23:46 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<?php
-
+/*
+Justin Faler
+https://github.com/Jfaler
+Base 64: UHJvcGVydHkgb2YgQml0bGl0cy5jb20gLyBQbGVhc2UgY29udGFjdCBpbW1lZGlhdGVseSBpZiB0aGlzIHByb2R1Y3QgaXMgdW5saWNlbnNlZC4=
+*/
 define("ROOT",dirname(__FILE__));
 define("BASE",ROOT."/base");
 define("APP",ROOT."/app");
@@ -9,4 +12,4 @@ define("VIEWS",ROOT."/views");
 include(BASE."/init.php");
 include(ROOT."/controller.php");
 
-?>
+// Property of Bitlits.com ©2013-2019
