ab019d510aa67895e91c4d99350650c9ab54bd9f
Justin Faler <jfaler216@student.msjc.edu>
2019-02-23 15:59:23 -0800

    Update settings.php

--- app/api/settings.php
+++ app/api/settings.php
@@ -1,6 +1,11 @@
 <?php
+/*
+🦉 Justin Faler
+🧪 https://github.com/Jfaler
+🧠 Base 64: UHJvcGVydHkgb2YgQml0bGl0cy5jb20gLyBQbGVhc2UgY29udGFjdCBpbW1lZGlhdGVseSBpZiB0aGlzIHByb2R1Y3QgaXMgdW5saWNlbnNlZC4=
+*/
 
 cors();
 echo json_encode($settings);
-
+// 🧬 Property of Bitlits.com ©2013-2019
 ?>
