0b2e6d421aa902d5f81994485f2944cdbe3938c0
Justin Faler <jfaler216@student.msjc.edu>
2019-02-23 15:53:52 -0800

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,4 +1,9 @@
 <?php
+/*
+🦉 Justin Faler
+🧪 https://github.com/Jfaler
+🧠 Base 64: UHJvcGVydHkgb2YgQml0bGl0cy5jb20gLyBQbGVhc2UgY29udGFjdCBpbW1lZGlhdGVseSBpZiB0aGlzIHByb2R1Y3QgaXMgdW5saWNlbnNlZC4=
+*/
 
 $dir = $var;
 $dir = array_filter($dir);
@@ -20,4 +25,5 @@ if($len>0){
 	header("Location: /index.html");
 }
 
+// 🧬 Property of Bitlits.com ©2013-2019
 ?>
